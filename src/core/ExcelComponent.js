27a0549a63d78462418d6680ad789f54cef23360
Vladislav <pestsov.JS@gmail.com>
2021-03-04 21:05:11 +0200

    Finish base table logic

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -1,21 +1,42 @@
 import {DomListener} from '@core/DomListener'
 
 export class ExcelComponent extends DomListener {
-    constructor($root, options = {}) {
-        super($root, options.listeners)
-        this.name = options.name || ''
-    }
-    // возврщаает шаблон компонента
-    toHTML() {
-        return ''
-    }
+  constructor($root, options = {}) {
+    super($root, options.listeners)
+    this.name = options.name || ''
+    this.emitter = options.emitter
+    this.unsubscribers = []
 
-    init() {
-        this.initDomListeners()
-    }
+    this.prepare()
+  }
+  // Настраиваем наш компонент до init
+  prepare() {}
 
-    destroy() {
-        this.removeDomListeners()
-    }
+  // Возвращает шаблон компонента
+  toHTML() {
+    return ''
+  }
+
+  // уведомляем слушателей про события event
+  $emit(event, ...args) {
+    this.emitter.emit(event, ...args)
+  }
+
+
+  // Подписываемся на события event
+  $on(event, fn) {
+    const unsub = this.emitter.subscribe(event, fn)
+    this.unsubscribers.push(unsub)
+  }
+
+  // инициализируем наш компонент, добавляем DOM слушателей
+  init() {
+    this.initDOMListeners()
+  }
+
+  // удаляем компонент, чистим слушателей.
+  destroy() {
+    this.removeDOMListeners()
+    this.unsubscribers.forEach(unsub => unsub())
+  }
 }
-
